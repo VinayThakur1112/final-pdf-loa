bd7a84f93e1916275d90047dc40f8ccc005ec6eb
Vinay Thakur <vinayrsinghthakur@gmail.com>
2025-12-28 19:16:37 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from sys import argv
-from app.email_listener.listener import listen
-from app.pdf_processor.subscriber import start_subscriber
 
 if __name__ == "__main__":
     
     if argv[1] == "email_listener":
-        listen()
+        from app.email_listener.listener import listen
+        try:
+            listen()
+        except KeyboardInterrupt:
+            print("📪 Email listener stopped by user")
     elif argv[1] == "pdf_processor":
-        start_subscriber()
+        from app.pdf_processor.subscriber import start_subscriber
+        try:
+            start_subscriber()
+        except KeyboardInterrupt:
+            print("📄 PDF processor subscriber stopped by user")
